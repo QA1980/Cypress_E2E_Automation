4f35c756ac7f52317c9543426d718c84e97d7624
Karim Haned <karimhan1980@gmail.com>
2020-07-15 14:10:49 -0400

    commit

---
+++ cypress/integration/E2E_Test/Testqa.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+
+
+/// <reference types="cypress" />
+
+describe('Valid login',()=> {
+  it('navigate to home page',()=>{
+  cy.visit('https://www.rewardbee.com')
+    cy.get('._1pcZ335O').click
+    cy.get('#bs-example-navbar-collapse-1 > ul > li:nth-child(1) > a').click()
+    cy.get('#email').type('testrewardbee12@gmail.com').blur()
+    cy.get('#password').type('test1234').blur()
+    cy.get('.btn-default').click()
+  })
+})
