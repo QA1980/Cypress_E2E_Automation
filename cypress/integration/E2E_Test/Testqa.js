45473111c22e59748b8ae615986fac115a8a36dd
karim <karimhan1980@gmail.com>
2020-07-17 10:20:48 -0400

    fix pwd

--- cypress/integration/E2E_Test/Testqa.js
+++ cypress/integration/E2E_Test/Testqa.js
@@ -10,7 +10,7 @@ describe('Valid login',()=> {
     cy.get('._1pcZ335O').click
     cy.get('#bs-example-navbar-collapse-1 > ul > li:nth-child(1) > a').click()
     cy.get('#email').type('testrewardbee12@gmail.com').blur()
-    cy.get('#password').type('test123').blur()
+    cy.get('#password').type('test1234').blur()
     cy.get('.btn-default').click()
   })
 })
