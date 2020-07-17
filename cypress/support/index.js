192a4b721f05c25f3b1b40e72849c269e2884655
karim <karimhan1980@gmail.com>
2020-07-17 16:52:20 -0400

    COMMIT

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -26,3 +26,8 @@ import './commands'
 module.exports= (on, config) => {
 
 }
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
