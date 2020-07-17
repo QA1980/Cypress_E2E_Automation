3f6c8433ad4bff79b3abebd3e98dc75cc60cd6fc
karim <karimhan1980@gmail.com>
2020-07-17 13:43:51 -0400

    changes

--- cypress/integration/E2E_Test/Testqa.js
+++ cypress/integration/E2E_Test/Testqa.js
@@ -2,11 +2,11 @@
 
 
 
-/// <reference types="cypress" />
+
 
 describe('Valid login',()=> {
   it('navigate to home page',()=>{
-  cy.visit('https://www.rewardbee.com')
+  cy.visit('https://www.rewardbee.com/')
     cy.get('._1pcZ335O').click
     cy.get('#bs-example-navbar-collapse-1 > ul > li:nth-child(1) > a').click()
     cy.get('#email').type('testrewardbee12@gmail.com').blur()
